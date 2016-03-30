2d8d24ccebd6804d60571b229cceeb1e7d6d972e
Mike Hawes <mikehawes@ww203083.wwnet.wne.edu>
2016-03-30 13:59:47 -0400

    this is the dictionary for the API request

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+thisDictionary = {"HPE": "HP Enterprise recognized"}
+
+
+def getFromDictionary(key) :
+    for aKey in thisDictionary:
+        return (thisDictionary[key])
+
+'''dictionary is imported into the program to send a string to the user's phone
+instead of the HPE key'''
+
+# created by Michael Hawes and Tony Tran
